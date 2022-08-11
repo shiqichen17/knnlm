b8352d3b16a6942a68f7da4700e86e521ef74f5e
shiqichen17 <1430411375@qq.com>
2022-08-11 13:34:57 +0800

    1

--- src/transformers/models/knngpt2/eval_lm.py
+++ src/transformers/models/knngpt2/eval_lm.py
@@ -16,45 +16,45 @@ def get_parser():
                         help='for FAISS, the number of lists to query')
     parser.add_argument('--k', default=1024, type=int,
                         help='number of nearest neighbors to retrieve')
-    parser.add_argument('--dstore-size', default=103227021, type=int,
+    parser.add_argument('--dstore_size', default=103227021, type=int,
                         help='number of items in the knnlm datastore')
-    parser.add_argument('--dstore-filename', type=str, default=None,
+    parser.add_argument('--dstore_filename', type=str, default=None,
                         help='File where the knnlm datastore is saved')
     parser.add_argument('--indexfile', type=str, default=None,
                         help='File containing the index built using faiss for knn')
     parser.add_argument('--lmbda', default=0.0, type=float,
                         help='controls interpolation with knn, 0.0 = no knn')
-    parser.add_argument('--knn-sim-func', default=None, type=str,
+    parser.add_argument('--knn_sim_func', default=None, type=str,
                         help='similarity function to use for knns')
-    parser.add_argument('--faiss-metric-type', default='l2', type=str,
+    parser.add_argument('--faiss_metric_type', default='l2', type=str,
                         help='the distance metric for faiss')
-    parser.add_argument('--no-load-keys', default=False, action='store_true',
+    parser.add_argument('--no_load_keys', default=False, action='store_true',
                         help='do not load keys')
-    parser.add_argument('--dstore-fp16', default=True, action='store_true',
+    parser.add_argument('--dstore_fp16', default=True, action='store_true',
                         help='if true, datastore items are saved in fp16 and int16')
-    parser.add_argument('--move-dstore-to-mem', default=False, action='store_true',
+    parser.add_argument('--move_dstore_to-mem', default=False, action='store_true',
                         help='move the keys and values for knn to memory')
     parser.add_argument('--decoder_embed_dim', default=False, action='store_true',
                         help='move the keys and values for knn to memory')
-    parser.add_argument('--gen-subset', default='test', metavar='SPLIT',
+    parser.add_argument('--gen_subset', default='test', metavar='SPLIT',
                        help='data subset to generate (train, valid, test)')
-    parser.add_argument('--output-word-probs', action='store_true',
+    parser.add_argument('--output_word_probs', action='store_true',
                        help='if set, outputs words and their predicted log probabilities to standard output')
-    parser.add_argument('--output-word-stats', action='store_true',
+    parser.add_argument('--output_word_stats', action='store_true',
                        help='if set, outputs word statistics such as word count, average probability, etc')
-    parser.add_argument('--context-window', default=0, type=int, metavar='N',
+    parser.add_argument('--context_window', default=0, type=int, metavar='N',
                        help='ensures that every evaluated token has access to a context of at least this size,'
                             ' if possible')
-    parser.add_argument('--softmax-batch', default=sys.maxsize, type=int, metavar='N',
+    parser.add_argument('--softmax_batch', default=sys.maxsize, type=int, metavar='N',
                        help='if BxT is more than this, will batch the softmax over vocab to this amount of tokens'
                             ' in order to fit into GPU memory')
-    parser.add_argument('--lm-eval', default=True, action='store_true',
+    parser.add_argument('--lm_eval', default=True, action='store_true',
                        help='helpful for certain ops that are only used during eval')
     parser.add_argument('--knnlm', action='store_true',
                        help='use the k-nearest neighbors language model')
-    parser.add_argument('--save-knnlm-dstore', action='store_true',
+    parser.add_argument('--save_knnlm_dstore', action='store_true',
                        help='save keys for the knnlm datastore')
-    parser.add_argument('--dstore-mmap', default=None, type=str,
+    parser.add_argument('--dstore_mmap', default=None, type=str,
                        help='If saving knnlm dstore, save keys and values to this file')
 
     args = parser.parse_args()
